784ef0b2596525ceb3079a2342303eccef54c6c2
koki_noguchi <koki.noguchi0202@gmail.com>
2020-07-05 12:54:44 +0900

    create files

---
+++ vue_part_4/app.js
@@ -0,0 +1,13 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: '',
+        description: 'This is textarea.',
+        framework: 'vue',
+        tools: {
+            webpack: true, babel: false, yarn: false
+        },
+        foods: ['🍩'],
+        lang: 'php'
+    }
+})
